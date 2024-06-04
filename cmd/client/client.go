5a639397ab996ec9ad225ee676520de9d974cdc7
Ola Karlsson <ola.karlsson@sdnit.se>
2024-06-04 15:04:15 +0200

    Stage before removing some stuff.

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-
-	pb "github.com/skalle/helloproto/hellopb"
+	// pb "github.com/skalle/helloproto/hellopb"
 )
 
 func main() {
 	fmt.Println("Hello world!")
-	pb := &pb.HelloRequest{
-		Name: "Opel Ascona",
-	}
+	// pb := &pb.HelloRequest{
+	// 	Name: "Opel Ascona",
+	// }
 
-	fmt.Println("Hello: %v", pb.Name)
+	// fmt.Println("Hello: %v", pb.Name)
+	fmt.Println("Hello: Le World")
 
 }
