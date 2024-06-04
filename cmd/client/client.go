d303faf44094054160216d3c8468cd74a2e14035
Ola Karlsson <ola.karlsson@sdnit.se>
2024-06-04 15:37:46 +0200

    Working!!

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	// pb "github.com/skalle/helloproto/hellopb"
+
+	pb "github.com/skalle/helloproto/proto/hellopb"
 )
 
 func main() {
-	// fmt.Println("Hello world!")
-	// pb := &pb.HelloRequest{
-	// 	Name: "Opel Ascona",
-	// }
+	fmt.Println("Hello world!")
+	pb := &pb.HelloRequest{
+		Name: "Opel Ascona",
+	}
 
-	// fmt.Println("Hello: %v", pb.Name)
+	fmt.Printf("Hello: %v\n", pb.Name)
 	fmt.Println("Hello: Le World")
 
 }
