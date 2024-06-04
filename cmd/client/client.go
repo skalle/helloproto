2127653f3892193f87a6990bacd816ed359ef00a
Ola Karlsson <ola.karlsson@sdnit.se>
2024-06-04 14:18:02 +0200

    Initial checkin

---
+++ cmd/client/client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	pb "github.com/skalle/helloproto/hellopb"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	pb := &pb.HelloRequest{
+		Name: "Opel Ascona",
+	}
+
+	fmt.Println("Hello: %v", pb.Name)
+
+}
